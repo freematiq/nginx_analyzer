e978454496f020e9fe43a61f73e7c604b48183ec
Tarasov Jaroslav <tarasovjaroslav@gmail.com>
2017-05-22 13:31:16 +0700

    v 0.3

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=lef',
+    'username' => 'lef',
+    'password' => '1441',
+    'charset' => 'utf8',
+];
