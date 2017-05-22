c5b466a463c9b2340535307e85b7eaacc9aa0261
Tarasov Jaroslav <tarasovjaroslav@gmail.com>
2017-05-22 11:14:06 +0700

    basic yii2

---
+++ views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>nginx_analyzer</h1>
+    </div>
+
+</div>
